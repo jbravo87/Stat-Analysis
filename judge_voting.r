d9b49ef12a3b52789f4877e491d8eca09271c620
J. Bravo <66102582+jbravo87@users.noreply.github.com>
2021-12-14 21:54:53 -0800

    update my model

--- judge_voting.r
+++ judge_voting.r
@@ -8,42 +8,99 @@ names(raw_data)
 # Vote1 using Judge1, Judge2, Judge3, and Category.
 # Per the README, 1 = liberal vote, 0 = conservative vote
 
-y <- as.factor(raw_data$Vote1)
-x1 <- as.factor(raw_data$Judge1)
-x2 <- as.factor(raw_data$Judge2)
-x3 <- as.factor(raw_data$Judge3)
-x4 <- as.factor(raw_data$Category)
-training_data = data.frame(y, x1, x4)
-  
-cor(training_data)
+# First filter will be by immigration in the category column.
+x1 <- data.frame( raw_data )
+x2 <- x1[x1$Category == "Immigration",]
+# The last variable stores just the cases that deal with immigration.
 
-model <- glm( y ~ x1 + x4, data = training_data, family = binomial )
-model2 <- glm( y ~ x1, data = training_data, family = binomial )
+# Next objective is to create a data frame with the three columns of interest.
+# Vote1, Judge1, and Category
+# For the binary 0/1 (conservative/liberal) dealing with nominal categorical variables.
+y1 <- factor(x2$Vote1, labels = c("Conservative", "Liberal"))
+
+
+y2 <- as.factor(x2$Judge1)
+
+# Following line gives all the names of the judges.
+levels(y2)
+
+# Now to unclass and assign each judge theor own respective number.
+y3 <- unclass(y2)
+print(y3)
+typeof(y3)
+
+y4 <- as.factor(y2)
+y4
+typeof(y4)
+
+# Create a final data frame to create model.
+training_data = data.frame(x2$Date, y1, y2)
+
+# Now to code the model using the two categorical variables.
+# logistical regression aka logit
+logit.model <- glm( y1 ~ y2, data = training_data, family = binomial )
 
 # summary statistics
-summary( model )
+summary( logit.model )
 
 # Will access just the coefficients for this fitted model.
-coef( model )
+coef( logit.model )
 
-summary(model)$coef
-summary(model)$coef[ , 4 ]
+# More summary data.
+summary(logit.model)$coef
+summary(logit.model)$coef[ , 4 ]
 
-# Predict function to predict the probability that the
+# Function to predict the probability that the
 # judge will vote liberal given predictor values.
+model.probs <- predict(logit.model, type = "response")
+model.probs
 
-model.probs <- predict(model, type = "response")
-model.probs[ 1:10 ]
+# To get the different categorical values.
+unique(y2)
 
-contrasts(training_data$y)
+# Attempting some intial plotting.
+plot(model.probs)
+plot(logit.model)
 
-model
+hist( y1 ~ y2, data = training_data, col = "steelblue" )
 
-# Going to convert the list of judges from as string or symbolic
-# to a new numeric list in order to plot
+cs <- cumsum(training_data$y2)
+
+# Following will give the number of occurences for each event.
+z1 = table(training_data$y1)
+
 
 judges_factor <- as.factor( training_data$x1 )
 judges_numeric <- as.numeric( training_data$x1 )
+length(judges_numeric)
+
+# The following is how to go about plotting a
+# logistical regression curve.
+
+#some_model <- glm( vs ~ hp, data = mtcars, family = binomial )
+#newdata <- data.frame( hp = seq(min(mtcars$hp), max(mtcars$hp ), len = 500))
+
+# Establish new data frame
+#newdf <- data.frame( judges_numeric = seq(min(judges_numeric), max(judges_numeric), len = 954))
+newdf2 <- data.frame( y, judges_numeric )
+newdf3 <- data.frame(judges = judges_numeric)
+
+#newdf$votes <- predict(model, newdf, type = "response" )
+#newdf2$votes <- predict(model, newdf2, type = "response" )
+#newdata$vs <- predict( some_model, newdata, type = "response" )
+newdf3$Vote <- predict( model2, newdf3, type = "response" )
+
+# Plotting Logic.
+#plot(vs~hp, data = mtcars, col = "steelblue" )
+#lines( vs~hp, newdata, lwd = 2)
+
+plot( judges_numeric ~ newdf2$y, data = training_data, col = "steelblue" )
+plot( judges_numeric ~ training_data$y )
+plot( training_data$y ~ judges_factor)
+plot( Vote ~ judges, data = newdf3, col = "steelblue" )
+summary(newdf3$judges)
+hist( training_data$x1 ~ training_data$y, data = newdf3, col = "steelblue" )
+typeof(newdf3$judges)
 length(judges_numeric)
 
 # The following is how to go about plotting a
