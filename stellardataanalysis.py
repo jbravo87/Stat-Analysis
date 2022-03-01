35442e87beb869216b996b85fbba1e46f64c1672
Jose Bravo <joepbravo@gmail.com>
2022-02-28 21:05:03 -0800

    New function to calculate means of other two columns in dataframe.

--- stellardataanalysis.py
+++ stellardataanalysis.py
@@ -29,4 +29,23 @@ df1 = df1.drop(df1.index[range(1)])
 # Next data frame will ne one with NA values removed
 df2 = df1.dropna()
 df2.reset_index(drop=True, inplace=True)
-plt.scatter(df2.orbitperiod, df2.eccentricity)
+# plt.scatter(df2.orbitperiod, df2.eccentricity)
+sample_df = df2.iloc[0:9]
+# Function to get the mean of a column from a dataframe
+def calc_mean(x_dataframe, column):
+    planetnames_list = list(set(x_dataframe['planetname'].values.tolist()))
+    header = list(x_dataframe.columns.values)
+    header.remove(column)
+    new_header = header
+    new_df = pd.DataFrame(columns = new_header)
+    for i,planet in enumerate(planetnames_list):
+        # results = x_dataframe[(x_dataframe['planetname'] == planet)].describe()
+        results = x_dataframe[(x_dataframe['planetname'] == planet)]
+        new_row = []
+        for column in new_header:
+            if column in ['planetname']:
+                new_row.append(planet)
+                continue
+            new_row.append(results[column].mean)
+        new_df.loc[i] = new_row
+    return new_df
