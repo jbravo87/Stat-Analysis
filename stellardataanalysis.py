e300d96ae579a37c7e86ed98d6a71aea400506e3
Jose Bravo <joepbravo@gmail.com>
2022-02-28 22:15:47 -0800

    Debugged the data types in the dataframe.

--- stellardataanalysis.py
+++ stellardataanalysis.py
@@ -30,7 +30,16 @@ df1 = df1.drop(df1.index[range(1)])
 df2 = df1.dropna()
 df2.reset_index(drop=True, inplace=True)
 # plt.scatter(df2.orbitperiod, df2.eccentricity)
-sample_df = df2.iloc[0:9]
+# sample_df = df2.iloc[0:9]
+sample_df = df2.iloc[0:789]
+#######
+# Notice that the data type for the last two columns 
+# is string and not double or numeric
+#######
+print(type(sample_df.iloc[0][1]))
+print(type(sample_df.iloc[0][2]))
+sample_df['orbitperiod'] = sample_df['orbitperiod'].astype(float)
+sample_df['eccentricity'] = sample_df['eccentricity'].astype(float)
 # Function to get the mean of a column from a dataframe
 def calc_mean(x_dataframe, column):
     planetnames_list = list(set(x_dataframe['planetname'].values.tolist()))
@@ -39,8 +48,8 @@ def calc_mean(x_dataframe, column):
     new_header = header
     new_df = pd.DataFrame(columns = new_header)
     for i,planet in enumerate(planetnames_list):
-        # results = x_dataframe[(x_dataframe['planetname'] == planet)].describe()
-        results = x_dataframe[(x_dataframe['planetname'] == planet)]
+        results = x_dataframe[(x_dataframe['planetname'] == planet)].describe()
+        #results = x_dataframe[(x_dataframe['planetname'] == planet)]
         new_row = []
         for column in new_header:
             if column in ['planetname']:
@@ -49,3 +58,10 @@ def calc_mean(x_dataframe, column):
             new_row.append(results[column].mean)
         new_df.loc[i] = new_row
     return new_df
+y1 = sample_df.groupby('planetname', as_index = False)['orbitperiod'].mean()
+y2 = sample_df.groupby('planetname', as_index = False)['eccentricity'].mean()
+# final_df = pd.concat([y1['orbitperiod'], y2['eccentricity']])
+result = y1.merge(y2, on=['planetname'])
+import seaborn as sns
+sns.distplot(result.orbitperiod)
+sns.distplot(result.eccentricity)
